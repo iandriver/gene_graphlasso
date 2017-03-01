132b21162292febfee7139505db920d504705657
iandriver <driver.ian@gmail.com>
2017-03-01 10:41:47 -0800

    fix make_graphlasso function

--- lasso_functions.py
+++ lasso_functions.py
@@ -139,111 +139,111 @@ def make_pcor_network(qt5_data):
     top_matrix_cell = top_matrix.T
     return top_matrix, top_matrix_cell, final_gene_list
 
-    def find_node_weight(top_matrix, final_gene_list):
-        mean_expr_dict = {}
-        for g in final_gene_list:
-            g_expr = top_matrix[g]
-            mean_expr_dict[g] = np.mean(g_expr)
+def find_node_weight(top_matrix, final_gene_list):
+    mean_expr_dict = {}
+    for g in final_gene_list:
+        g_expr = top_matrix[g]
+        mean_expr_dict[g] = np.mean(g_expr)
 
-        node_weights =[int(500*round(v)) for k,v in mean_expr_dict.items()]
-        return node_weights
+    node_weights =[int(500*round(v)) for k,v in mean_expr_dict.items()]
+    return node_weights
 
-    def make_graphlasso(top_matrix, final_gene_list):
-        gl = covariance.GraphLassoCV()
-        gene_data = scale(top_matrix.as_matrix())
+def make_graphlasso(top_matrix, final_gene_list):
+    gl = covariance.GraphLassoCV()
+    gene_data = scale(top_matrix.as_matrix())
 
-        gl.fit(gene_data)
-        _, labels = cluster.affinity_propagation(gl.covariance_)
-        n_labels = labels.max()
-        names = np.array(final_gene_list)
+    gl.fit(gene_data)
 
-        node_position_model = manifold.LocallyLinearEmbedding(
-            n_components=2, eigen_solver='dense', n_neighbors=7)
+    names = np.array(final_gene_list)
 
-        embedding = node_position_model.fit_transform(gene_data.T).T
+    node_position_model = manifold.LocallyLinearEmbedding(
+        n_components=2, eigen_solver='dense', n_neighbors=7)
 
-        return gl, embedding, names
+    embedding = node_position_model.fit_transform(gene_data.T).T
 
-    def plot_networks(gl, embedding, names, node_weights, qt5_data):
+    return gl, embedding, names
 
-        path_to_file = qt5_data.filepath.text()
-        plt.figure(1, facecolor='w', figsize=(10, 8))
-        plt.clf()
-        ax = plt.axes([0., 0., 1., 1.])
-        plt.axis('off')
+def plot_networks(gl, embedding, names, node_weights, qt5_data):
+    _, labels = cluster.affinity_propagation(gl.covariance_)
+    n_labels = labels.max()
+    path_to_file = qt5_data.filepath.text()
+    plt.figure(1, facecolor='w', figsize=(10, 8))
+    plt.clf()
+    ax = plt.axes([0., 0., 1., 1.])
+    plt.axis('off')
 
-        # Display a graph of the partial correlations
-        partial_correlations = gl.precision_.copy()
-        d = 1 / np.sqrt(np.diag(partial_correlations))
-        partial_correlations *= d
-        partial_correlations *= d[:, np.newaxis]
-        non_zero = (np.abs(np.triu(partial_correlations, k=1)) > 0.02)
+    # Display a graph of the partial correlations
+    partial_correlations = gl.precision_.copy()
+    d = 1 / np.sqrt(np.diag(partial_correlations))
+    partial_correlations *= d
+    partial_correlations *= d[:, np.newaxis]
+    non_zero = (np.abs(np.triu(partial_correlations, k=1)) > 0.02)
 
-        # Plot the nodes using the coordinates of our embedding
-        cm = plt.cm.get_cmap('spectral')
-        #cmap = [cm(1.*i/len(embedding[0])) for i in range(len(embedding[0]))]
-
-
-        plt.scatter(embedding[0], embedding[1], s=100 * d ** 2, c=labels,
-                    cmap=cm)
-
-        # Plot the edges
-        start_idx, end_idx = np.where(non_zero)
-        #a sequence of (*line0*, *line1*, *line2*), where::
-        #            linen = (x0, y0), (x1, y1), ... (xm, ym)
-        segments = [[embedding[:, start], embedding[:, stop]]
-                    for start, stop in zip(start_idx, end_idx)]
-        values = np.abs(partial_correlations[non_zero])
-        lc = collections.LineCollection(segments,
-                            zorder=0, cmap=plt.cm.hot_r,
-                            norm=plt.Normalize(0, .7 * values.max()))
-        lc.set_array(values)
-        lc.set_linewidths(15 * values)
-        ax.add_collection(lc)
-
-        # Add a label to each node. The challenge here is that we want to
-        # position the labels to avoid overlap with other labels
-        for index, (name, label, (x, y)) in enumerate(
-                zip(names, labels, embedding.T)):
-
-            dx = x - embedding[0]
-            dx[index] = 1
-            dy = y - embedding[1]
-            dy[index] = 1
-            this_dx = dx[np.argmin(np.abs(dy))]
-            this_dy = dy[np.argmin(np.abs(dx))]
-            if this_dx > 0:
-                horizontalalignment = 'left'
-                x = x + .002
-            else:
-                horizontalalignment = 'right'
-                x = x - .002
-            if this_dy > 0:
-                verticalalignment = 'bottom'
-                y = y + .002
-            else:
-                verticalalignment = 'top'
-                y = y - .002
-            plt.text(x, y, name, size=10,
-                     horizontalalignment=horizontalalignment,
-                     verticalalignment=verticalalignment,
-                     bbox=dict(facecolor='w',
-                               edgecolor=plt.cm.nipy_spectral(label / float(n_labels)),
-                               alpha=.6))
-
-        plt.xlim(embedding[0].min() - .15 * embedding[0].ptp(),
-                 embedding[0].max() + .10 * embedding[0].ptp(),)
-        plt.ylim(embedding[1].min() - .03 * embedding[1].ptp(),
-                 embedding[1].max() + .03 * embedding[1].ptp())
+    # Plot the nodes using the coordinates of our embedding
+    cm = plt.cm.get_cmap('spectral')
+    #cmap = [cm(1.*i/len(embedding[0])) for i in range(len(embedding[0]))]
 
 
-        plt.savefig(os.path.join(os.path.dirname(path_to_file),'linedraw_graph.pdf'),bbox_inches="tight")
+    plt.scatter(embedding[0], embedding[1], s=100 * d ** 2, c=labels,
+                cmap=cm)
 
-        prec_sp = gl.precision_
+    # Plot the edges
+    start_idx, end_idx = np.where(non_zero)
+    #a sequence of (*line0*, *line1*, *line2*), where::
+    #            linen = (x0, y0), (x1, y1), ... (xm, ym)
+    segments = [[embedding[:, start], embedding[:, stop]]
+                for start, stop in zip(start_idx, end_idx)]
+    values = np.abs(partial_correlations[non_zero])
+    lc = collections.LineCollection(segments,
+                        zorder=0, cmap=plt.cm.hot_r,
+                        norm=plt.Normalize(0, .7 * values.max()))
+    lc.set_array(values)
+    lc.set_linewidths(15 * values)
+    ax.add_collection(lc)
 
-        save_network_graph( -prec_sp + np.diag( np.diagonal( prec_sp) ), names, os.path.join(os.path.dirname(path_to_file),"LargeNetworkNo_SP.pdf"), title="spring_sp_prec_test",layout="spring", scale= 10, node_weight=node_weights, weight = lambda x: abs(5*x)**(2.5))
-        save_network_graph( gl.covariance_, names, os.path.join(os.path.dirname(path_to_file),"cov_diagram.pdf"), node_weight=node_weights, title="cov_test", scale = 8, layout = "spring" )
-        save_network_graph( gl.precision_, names, os.path.join(os.path.dirname(path_to_file),"precision.pdf") , title="Precision Matrix Network", layout="spring", node_weight= node_weights)
+    # Add a label to each node. The challenge here is that we want to
+    # position the labels to avoid overlap with other labels
+    for index, (name, label, (x, y)) in enumerate(
+            zip(names, labels, embedding.T)):
+
+        dx = x - embedding[0]
+        dx[index] = 1
+        dy = y - embedding[1]
+        dy[index] = 1
+        this_dx = dx[np.argmin(np.abs(dy))]
+        this_dy = dy[np.argmin(np.abs(dx))]
+        if this_dx > 0:
+            horizontalalignment = 'left'
+            x = x + .002
+        else:
+            horizontalalignment = 'right'
+            x = x - .002
+        if this_dy > 0:
+            verticalalignment = 'bottom'
+            y = y + .002
+        else:
+            verticalalignment = 'top'
+            y = y - .002
+        plt.text(x, y, name, size=10,
+                 horizontalalignment=horizontalalignment,
+                 verticalalignment=verticalalignment,
+                 bbox=dict(facecolor='w',
+                           edgecolor=plt.cm.nipy_spectral(label / float(n_labels)),
+                           alpha=.6))
+
+    plt.xlim(embedding[0].min() - .15 * embedding[0].ptp(),
+             embedding[0].max() + .10 * embedding[0].ptp(),)
+    plt.ylim(embedding[1].min() - .03 * embedding[1].ptp(),
+             embedding[1].max() + .03 * embedding[1].ptp())
+
+
+    plt.savefig(os.path.join(os.path.dirname(path_to_file),'linedraw_graph.pdf'),bbox_inches="tight")
+
+    prec_sp = gl.precision_
+
+    save_network_graph( -prec_sp + np.diag( np.diagonal( prec_sp) ), names, os.path.join(os.path.dirname(path_to_file),"LargeNetworkNo_SP.pdf"), title="spring_sp_prec_test",layout="spring", scale= 10, node_weight=node_weights, weight = lambda x: abs(5*x)**(2.5))
+    save_network_graph( gl.covariance_, names, os.path.join(os.path.dirname(path_to_file),"cov_diagram.pdf"), node_weight=node_weights, title="cov_test", scale = 8, layout = "spring" )
+    save_network_graph( gl.precision_, names, os.path.join(os.path.dirname(path_to_file),"precision.pdf") , title="Precision Matrix Network", layout="spring", node_weight= node_weights)
 
 def community_cluster(cov_sp, symbols):
 	G = nx.Graph( cov_sp )
